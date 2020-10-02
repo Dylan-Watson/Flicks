a3626ffe0d34834e7d1b142aae562c08f23d0182
Dylan Watson <watson.dylan@centaurisoftware.co>
2020-10-02 02:38:20 -0500

    Init commit. Initial setup of flask

---
+++ index.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+class ConfigClass(object):
+    SECRET_KEY = b'p\xe3\\l\xd5\xee\\6\xaa\xc4\xbc\xd0n\x95\xea\xfe\x00z\x82[t\x1bs\x85? \xe11\x98\xf9\xda@\x83\x1f\xa0"\xa3\xdf\xe1z\xe6R\xcc/\xafM5z\xdcY\xbe\xa9tqvj\x85\xe4\xe1\xaf\x9b\x07\x88.'
+
+app.config.from_object(__name__+'.ConfigClass')
+
+# * Test route
+@app.route('/',methods=['GET'])
+def testRoute():
+    return render_template('test.html')
---
+++ run.bat
@@ -0,0 +1,4 @@
+venv\Scripts\activate
+set FLASK_APP=index.py
+set FLASK_ENV=development
+flask run
